c76fa5fcbbe907605fa986305969ddadd0a00eea
csaxton-ls <craig.saxton@legitscript.com>
2024-10-16 09:37:06 -0700

    feat: establish basic proposal functionality

---
+++ proposals/__tests__/index.spec.js
@@ -0,0 +1,12 @@
+
+
+describe("proposals.index", () => {
+    describe("foo", () => {
+
+        it("should return 'bar'", () => {
+            const index = require("../index");
+            expect(index.foo()).toBe("bar");
+        });
+
+    });
+});
